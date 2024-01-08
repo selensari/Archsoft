540011da3cbe4ba8f21b94d55daffe3d2a32b6d8
selensari <selensari25@gmail.com>
2024-01-08 03:36:31 +0300

    selenbaba

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('loginButton').addEventListener('click', function () {
+    alert('Giriş Yap butonuna tıklandı!');
+});
+
+document.getElementById('registerButton').addEventListener('click', function () {
+    alert('Kayıt Ol butonuna tıklandı!');
+});
+
+
+
